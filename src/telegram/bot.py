caccf9903c827eec3d64097920243a5a39ec61e4
Artemiy Kochergin <kocherginartem@mail.ru>
2024-06-02 11:30:05 +0300

    improved error tolerance

--- src/telegram/bot.py
+++ src/telegram/bot.py
@@ -6,6 +6,12 @@ from model.event import Event, to_str
 from extractor.stanford_extractor import extract_events
 from telegram.messages import HELP_MESSAGE, START_MESSAGE, PROCESSING_MESSAGE
 
+log.basicConfig(filename="logs.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=log.DEBUG)
+
 
 def convert_events(events: list[Event]) -> str:
     result = ""
@@ -22,21 +28,31 @@ markup = telebot.types.InlineKeyboardMarkup()
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == '/start':
-        bot.send_message(message.from_user.id, START_MESSAGE)
-    elif message.text == '/help':
-        bot.send_message(message.from_user.id, HELP_MESSAGE, reply_markup=markup, parse_mode='MarkdownV2')
-    else:
-        bot.send_message(message.from_user.id, PROCESSING_MESSAGE)
-        try:
-            response = convert_events(extract_events(message.text))
-            if response:
-                bot.send_message(message.from_user.id,  "*Here\\'s the list of events*\\! ⬇️\n\n" + response, reply_markup=markup, parse_mode='MarkdownV2')
-            else:
-                bot.send_message(message.from_user.id, "No events found in text.")
-        except Exception as error:
-            log.error(error)
-            bot.send_message(message.from_user.id, 'An error occurred, please try another text.')
+    try:
+        if message.text == '/start':
+            bot.send_message(message.from_user.id, START_MESSAGE)
+        elif message.text == '/help':
+            bot.send_message(message.from_user.id, HELP_MESSAGE, reply_markup=markup, parse_mode='MarkdownV2')
+        else:
+            bot.send_message(message.from_user.id, PROCESSING_MESSAGE)
+            try:
+                response = convert_events(extract_events(message.text))
+                if response:
+                    bot.send_message(message.from_user.id, "*Here\\'s the list of events*\\! ⬇️\n\n" + response,
+                                     reply_markup=markup, parse_mode='MarkdownV2')
+                    log.info(f"Successfully extracted events from text. Text: {message.text}")
+                else:
+                    log.info(f"No events found in text. Text: {message.text}")
+                    bot.send_message(message.from_user.id, "No events found in text.")
+            except Exception as error:
+                log.error(error)
+                bot.send_message(message.from_user.id, 'An error occurred, please try another text.')
+    except Exception as e:
+        log.error(e)
 
 
-bot.polling(none_stop=True, interval=0)
+while True:
+    try:
+        bot.polling(non_stop=True, interval=0)
+    except Exception as e:
+        log.error(e)

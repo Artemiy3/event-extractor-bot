31634f530e06a0d0c4b1a815bdb275cfab4159db
Artemiy Kochergin <kocherginartem@mail.ru>
2024-06-05 20:41:29 +0300

    update dataset and metrics script

--- src/measurement/metrics.py
+++ src/measurement/metrics.py
@@ -10,8 +10,12 @@ def convert_event(event: Event) -> str:
 
 
 def measure():
+    # event_extractor = EventExtractor()
     with open("event_extraction_dataset.txt", "r") as f:
         lines = f.readlines()
+        index = lines.index("__stop__\n")
+        lines = lines[0:index]
+        print(lines)
 
         sentences = []
         expected_events = []
@@ -21,13 +25,22 @@ def measure():
             dot_index = line.index(".")
 
             sent = line[0:dot_index + 1]
+            sentences.append(sent)
+
             event_str = line[dot_index + 3:len(line) - 1]
-
             actor, action, time, place = event_str.split(", ")
-
             event = Event(actor, action, time, place)
 
-            sentences.append(sent)
+            # raw_events = events_str.split(';')
+            #
+            # sent_events = []
+            #
+            # for raw_event in raw_events:
+            #     event_str = raw_event[1:len(raw_event) - 1]
+            #     actor, action, time, place = event_str.split(", ")
+            #     event = Event(actor, action, time, place)
+            #     sent_events.append(event)
+
             expected_events.append(event)
 
         predicted_events = []
@@ -36,12 +49,42 @@ def measure():
             print(i)
             try:
                 predicted_event_list = extract_events(sent)
+                # sent_predicted_events = []
                 if not predicted_event_list:
                     predicted_events.append(Event("", "", "", ""))
+                    # for _ in expected_events[i]:
+                    #     sent_predicted_events.append(Event("", "", "", ""))
                 else:
                     predicted_events.append(predicted_event_list[0])
+                    # sent_predicted_events.extend(predicted_event_list)
+                    # while len(sent_predicted_events) < len(expected_events[i]):
+                    #     sent_predicted_events.append(Event("", "", "", ""))
+                    # while len(sent_predicted_events) > len(expected_events[i]):
+                    #     expected_events[i].append(Event("", "", "", ""))
+                    # predicted_events.append(sent_predicted_events)
+
             except IndexError:
                 print("ERROR: " + sent)
+
+        # flattened_expected_events = []
+        # for event_list in expected_events:
+        #     for event in event_list:
+        #         flattened_expected_events.append(event)
+        #
+        # flattened_predicted_events = []
+        # for event_list in predicted_events:
+        #     for event in event_list:
+        #         flattened_predicted_events.append(event)
+
+        # with open("extracted_events.txt", "w") as fw:
+        #     lines_to_write = []
+        #     for event in predicted_events:
+        #         actor_str = event.actor if event.actor is not None else ""
+        #         action_str = event.action if event.action is not None else ""
+        #         time_str = event.time if event.time is not None else ""
+        #         place_str = event.place if event.place is not None else ""
+        #         lines_to_write.append(f"{{{actor_str}, {action_str}, {time_str}, {place_str}}}\n")
+        #     fw.writelines(lines_to_write)
 
         predicted_actors = list(map(lambda e: e.actor if e.actor is not None else "", predicted_events))
         predicted_actions = list(map(lambda e: e.action if e.action is not None else "", predicted_events))
